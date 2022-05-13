3ffa2c691660dc554aa38eff4701e5a155d45a09
Pablo Correa <pablocorrea523@gmail.com>
2022-05-13 16:04:01 -0400

    ajustes de responsibidad y formulario

---
+++ js/sendEmail.js
@@ -0,0 +1,16 @@
+const $form = document.querySelector("#form");
+const $buttonMailto = document.querySelector("#mailto");
+
+$form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  $buttonMailto.setAttribute(
+    "href",
+    `mailto:pablocorrea@gmail.com?subject=${form.get("name")}${form.get(
+      "email"
+    )}&body=${form.get("message")}`
+  );
+  $buttonMailto.clicK();
+}
